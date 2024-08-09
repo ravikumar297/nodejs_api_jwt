0aa109c8040ca7c80213e2088a307ecf3bd06b38
ravikumar297 <msmnr29@gmail.com>
2024-08-09 20:54:59 +0530

    cors enables

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const userRoutes = require('./api/user/user.router');
 
 app.use(express.json()); 
 
+app.use(cors());
 app.use('/api/users', userRoutes);
 app.listen(process.env.APP_PORT, () => {
     console.log("Working fine now please check in port : ", process.env.APP_PORT)
